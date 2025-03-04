24fb7dae6b8efa9a86336fb50d08d42e0622b411
alayyubi212 <sholahuddinalayyubi212@gmail.com>
2025-03-04 08:30:19 +0700

    praktikum 02

---
+++ praktikum-2a/data-form-register.php
@@ -0,0 +1,18 @@
+<?php
+$ar_prodi = [
+    'SI' => 'sistem informasi',
+    'TI' => 'teknik informatika',
+    'BD' => 'bisnis digital',
+
+];
+
+$ar_skill = [
+    'HTML' => 10,
+    'CSS' => 10,
+    'JavaSrcipt' => 20,
+    'RWD Bootstarp' => 20,
+    'PHP' => 30,
+    'python' => 30,
+    'Java' => 50
+];
+$ar_Domisili = ["Jakarta" , "Bekasi" ,"Tanggerang" , "Depok" ,"Lainnya"];
